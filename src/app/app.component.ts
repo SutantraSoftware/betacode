8a53872884d01eb6749cbb2bd9e3e3aab0799869
Sai Deshoj <sai.deshoj@kennametal.com>
2025-02-05 20:45:12 +0530

    Home page commit changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'beta-code';
+  constructor(library: FaIconLibrary) {
+    library.addIcons();
+  }
 }
